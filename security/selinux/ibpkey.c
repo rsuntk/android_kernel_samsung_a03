5592a48eef9cf760c13ec72cc0880f1063689535
Rissu <90097027+rsuntk@users.noreply.github.com>
2024-06-25 11:17:33 +0000

    selinux: import changes from v4.14.212

--- security/selinux/ibpkey.c
+++ security/selinux/ibpkey.c
@@ -161,9 +161,11 @@ static int sel_ib_pkey_sid_slow(u64 subnet_prefix, u16 pkey_num, u32 *sid)
 	 * is valid, it just won't be added to the cache.
 	 */
 	new = kzalloc(sizeof(*new), GFP_ATOMIC);
-	if (!new)
+	if (!new) {
+		ret = -ENOMEM;
 		goto out;
-
+	}
+	
 	new->psec.subnet_prefix = subnet_prefix;
 	new->psec.pkey = pkey_num;
 	new->psec.sid = *sid;
